be093852d0e216d306019eef3099db3f59dbc603
Debangan Dey <ddey1@jhu.edu>
2018-09-24 22:09:11 -0400

    Trying one dimensional simulation to see the error, still huge errors!

--- Code/Simulation.R
+++ Code/Simulation.R
@@ -22,38 +22,87 @@ library(BRISC)
 
 ##### Simulating from multivariate Matern model
 cor.mat <- matrix(nc=3, c(1, 0.5, 0.2, 0.5, 1, 0.6, 0.2, 0.6, 1))
-sigma.sq <- 2
-phi <- 0.3
+sigma.sq <- 1
+phi <- 3
 tau.sq <- 0
+mu <- 1
 
 model <- RMparswmX(nudiag=c(0.5, 0.5, 0.5), rho=cor.mat, scale=1/phi, var=sigma.sq) 
 
-set.seed(1)
-#n <- 1000
-coords <- cbind(seq(0,1,length.out = 10),seq(length.out = 10))
+#set.seed(10)
+#n <- 200
+#coords <- cbind(runif(n,0,1),runif(n,0,1))
+#Setting up grid
+coords <- cbind(seq(0,1,length.out = 20), seq(0,1,length.out = 20))
 
+#Setting nugget variance to be 0
 tau.sq <- 0
 w <- RFsimulate(model = model, x=coords[,1], y=coords[,2]) 
-e <- matrix(rnorm(ncol(w)*nrow(w),1,sd=sqrt(tau.sq)),ncol=ncol(w))
+##Setting mean to be 1
+e <- matrix(rnorm(ncol(w)*nrow(w),mu,sd=sqrt(tau.sq)),ncol=ncol(w))
 
 z <- w@data + e
+# 
+# #estimating marginal Materns with likfit
+# M <- list()
+# for(i in 1:ncol(z)){
+# M[[i]] <- likfit(coords=coordinates(w), data=z[,i], cov.model = "matern", fix.kappa=FALSE,ini = c(1.3, 3), fix.nugget = TRUE, nugget=tau.sq)
+# }
 
-#estimating marginal 
+### Estimating marginal materns wth
 M <- list()
 for(i in 1:ncol(z)){
-M[[i]] <- likfit(coords=coordinates(w), data=z[,i], cov.model = "matern", fix.kappa=FALSE,ini = c(1, 0.3), fix.nugget = TRUE, nugget=tau.sq)
+M[[i]] <- BRISC_estimation(coordinates(w), x=as.matrix(rep(1,length(z[,i]))), y=z[,i])
 }
-
-
-### Estimating marginal materns
-#res1 <- BRISC_estimation(coordinates(w), x=as.matrix(rep(1,length(z[,1]))), y=z[,1])
 
 d = as.matrix(dist(coordinates(w),method = "euclidean",diag = T,upper = T))
 
 Diag.S <- list()
 
+# for(i in 1:ncol(z)){
+# Diag.S[[i]]<- matrix(Matern(as.numeric(d), alpha=1/M[[i]]$phi, smoothness=M[[i]]$kappa, phi=M[[i]]$sigmasq), ncol=nrow(d))
+# }
+
+###From BRISC estimation
 for(i in 1:ncol(z)){
-Diag.S[[i]]<- matrix(Matern(as.numeric(d), alpha=1/M[[i]]$phi, smoothness=M[[i]]$kappa, phi=M[[i]]$sigmasq), ncol=nrow(d))
+  Diag.S[[i]]<- matrix(Matern(as.numeric(d), alpha=1/M[[i]]$Theta[3], smoothness=0.5, phi=M[[i]]$Theta[1]), ncol=nrow(d))
+}
+
+
+
+######Trying one dimension to check the error##########
+sigma.sq <- 1
+phi <- 1
+tau.sq <- 0
+mu <- 1
+
+model <- RMparswmX(nudiag=0.5, rho=1) 
+model <- RMmatern(nu=0.5)
+
+#set.seed(10)
+#n <- 200
+#coords <- cbind(runif(n,0,1),runif(n,0,1))
+#Setting up grid
+coords <- cbind(seq(0,1,length.out = 20), seq(0,1,length.out = 20))
+
+#Setting nugget variance to be 0
+tau.sq <- 0
+w <- RFsimulate(model = model, x=coords[,1], y=coords[,2]) 
+##Setting mean to be 1
+e <- matrix(rnorm(ncol(w)*nrow(w),mu,sd=sqrt(tau.sq)),ncol=ncol(w))
+
+z <- w@data + e
+# 
+#estimating marginal Materns with likfit
+M <- list()
+for(i in 1:ncol(z)){
+M[[i]] <- likfit(coords=coordinates(w), data=z[,i], cov.model = "matern", fix.kappa=FALSE,ini = c(1.3, 3), fix.nugget = TRUE, nugget=tau.sq)
+}
+
+### Estimating marginal materns wth
+M <- list()
+for(i in 1:ncol(z)){
+  M[[i]] <- BRISC_estimation(coordinates(w), x=as.matrix(rep(1,length(z[,i]))), y=z[,i])
 }
 
 
